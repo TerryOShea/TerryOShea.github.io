dcd8029e155fa690b5faf80de02a5ef3b213424c
Terry O'Shea <terrykoshea@gmail.com>
2016-10-28 11:52:21 -0400

    update work page

--- index.js
+++ index.js
@@ -1,39 +1,56 @@
 $(document).ready(function() {
+    
+  const $page = $(".page"),
+        $aboutBtn = $(".header-about"),
+        $homeBtn = $(".header-name"), 
+        $workBtn = $(".header-work"), 
+        $flipper = $(".flipper"), 
+        $forwardArrow = $(".forward-arrow"), 
+        $backArrow = $(".back-arrow"), 
+        $skewer = $(".skewer"), 
+        $workBox = $(".work-box"), 
+        $workItemFront = $(".work-item-front"), 
+        $workItemBack = $(".work-item-back");
   
-  const $page = $('.page'),
-      $headerBtn = $('.header-btn'), 
-      $aboutBtn = $('.header-about'),
-      $homeBtn = $('.header-name'), 
-      $workBtn = $('.header-work');
+  const displayedWork = [["<img src='/images/dungeoncrawler.gif' class='work-gif'>", "Information about this project here"], 
+  ["<img src='/images/simongame.gif' class='work-gif'>", "Information about this project here"], 
+  ["<img src='/images/conway.gif' class='work-gif'>", "Information about this project here"], 
+  ["<img src='/images/d3-graphs.gif' class='work-gif'>", "Information about this project here"]];
   
-  const canvas = document.getElementById('homecanvas'), 
-        ctx = canvas.getContext('2d'), 
-        pattern = document.createElement('canvas'),
-        pctx = pattern.getContext('2d');
+  const canvas = document.getElementById("homecanvas"), 
+        ctx = canvas.getContext("2d"), 
+        pattern = document.createElement("canvas"),
+        pctx = pattern.getContext("2d");
   pattern.width = 50;
   pattern.height = 50;
-  var patternCt = 0;
+  
+  var patternCt = 0, 
+      displayCt = 0, 
+      rotationCt = 0;
   
   newPattern();
-  var rotationCt = 180;
-  $('.flipper').on('click', function() {
-    $(this).css('transform', 'rotateX(' + rotationCt + 'deg)');
-    rotationCt += 180;
-  });
+  showWork();
   
-  $headerBtn.on('click', function() {
-    let oldpage = $('.disabled'), 
+  $flipper.on("click", flipit);
+  
+  function flipit() {
+    rotationCt += 180;
+    $flipper.css("transform", "rotateX(" + rotationCt + "deg)");
+  }
+  
+  $(".header-btn").on("click", function() {
+    let oldpage = $(".disabled"), 
         newpage = $(this);
     
     setTimeout(function() {
-      newpage.addClass('disabled');
-      oldpage.removeClass('disabled');
-      $('.arrow').remove();
-      if (newpage.hasClass('header-about')) {
+      newpage.addClass("disabled");
+      oldpage.removeClass("disabled");
+      $(".arrow").remove();
+      if (newpage.hasClass("header-about")) {
         $homeBtn.append("<span class='arrow right-arrow'> &gt;</span>");
         $workBtn.append("<span class='arrow right-arrow'>&gt;&gt;</span>");
       }
-      else if (newpage.hasClass('header-work')) {
+      else if (newpage.hasClass("header-work")) {
         $homeBtn.prepend("<span class='arrow right-arrow'> &lt; </span>");
         $aboutBtn.prepend("<span class='arrow right-arrow'>&lt;&lt;</span>");
       }
@@ -43,23 +60,23 @@ $(document).ready(function() {
       }
     }, 1000);
     
-    if (newpage.hasClass('header-about')) {
-      if (oldpage.hasClass('header-name')) $page.css('animation', 'hometoabout 2s forwards');
+    if (newpage.hasClass("header-about")) {
+      if (oldpage.hasClass("header-name")) $page.css("animation", "hometoabout 2s forwards");
       else {
-        $page.css('animation', 'worktoabout 2s forwards');
+        $page.css("animation", "worktoabout 2s forwards");
         newPattern();
       }
     }
-    else if (newpage.hasClass('header-work')) {
-      if (oldpage.hasClass('header-name')) $page.css('animation', 'hometowork 2s forwards');
+    else if (newpage.hasClass("header-work")) {
+      if (oldpage.hasClass("header-name")) $page.css("animation", "hometowork 2s forwards");
       else { 
-        $page.css('animation', 'abouttowork 2s forwards');
+        $page.css("animation", "abouttowork 2s forwards");
         newPattern()
       }
     }
     else {
-      if (oldpage.hasClass('header-about')) $page.css('animation', 'abouttohome 2s forwards');
-      else $page.css('animation', 'worktohome 2s forwards');
+      if (oldpage.hasClass("header-about")) $page.css("animation", "abouttohome 2s forwards");
+      else $page.css("animation", "worktohome 2s forwards");
       newPattern();
     }
   });
@@ -67,22 +84,22 @@ $(document).ready(function() {
   function newPattern() {
     switch(patternCt) {
       case 0: 
-        pctx.fillStyle = 'skyblue';
+        pctx.fillStyle = "skyblue";
         pctx.fillRect(0, 0, 50, 50);
-        pctx.fillStyle = 'floralwhite';
+        pctx.fillStyle = "floralwhite";
         pctx.fillRect(0, 0, 50, 25);
         break;
       case 1: 
-        pctx.fillStyle = 'tomato';
+        pctx.fillStyle = "tomato";
         pctx.fillRect(0, 0, 50, 50);
-        pctx.fillStyle = 'papayawhip';
+        pctx.fillStyle = "papayawhip";
         pctx.beginPath();
         pctx.moveTo(25, 0);
         pctx.lineTo(50, 25);
         pctx.lineTo(25, 50);
         pctx.lineTo(0, 25);
         pctx.fill();
-        pctx.strokeStyle = 'lightgray';
+        pctx.strokeStyle = "lightgray";
         pctx.beginPath();
         pctx.moveTo(0, 0);
         pctx.lineTo(50, 50);
@@ -93,9 +110,9 @@ $(document).ready(function() {
         pctx.stroke();
         break;
       case 2: 
-        pctx.fillStyle = 'steelblue';
+        pctx.fillStyle = "steelblue";
         pctx.fillRect(0, 0, 50, 50);
-        pctx.strokeStyle = 'lightsteelblue';
+        pctx.strokeStyle = "lightsteelblue";
         pctx.beginPath();
         pctx.moveTo(25, 0);
         pctx.lineTo(25, 50);
@@ -114,9 +131,9 @@ $(document).ready(function() {
         pctx.stroke();
         break;
       case 3: 
-        pctx.fillStyle = 'lightpink';
+        pctx.fillStyle = "lightpink";
         pctx.fillRect(0, 0, 50, 50);
-        pctx.fillStyle = 'lightgreen';
+        pctx.fillStyle = "lightgreen";
         pctx.beginPath();
         pctx.moveTo(0, 0);
         pctx.lineTo(0, 50);
@@ -124,9 +141,9 @@ $(document).ready(function() {
         pctx.fill();
         break;
       case 4: 
-        pctx.fillStyle = 'mediumturquoise';
+        pctx.fillStyle = "mediumturquoise";
         pctx.fillRect(0, 0, 50, 50);
-        pctx.strokeStyle = 'mistyrose';
+        pctx.strokeStyle = "mistyrose";
         pctx.lineWidth = 4;
         pctx.beginPath();
         pctx.arc(25, 0, 12.5, 0, .5*Math.PI);
@@ -135,13 +152,13 @@ $(document).ready(function() {
         pctx.stroke();
         break;
       case 5: 
-        pctx.fillStyle = 'plum';
+        pctx.fillStyle = "plum";
         pctx.fillRect(0, 0, 50, 50);
-        pctx.fillStyle = 'lemonchiffon';
+        pctx.fillStyle = "lemonchiffon";
         pctx.beginPath();
         pctx.arc(25, 25, 25, 0, 2*Math.PI);
         pctx.fill();
-        pctx.fillStyle = 'plum';
+        pctx.fillStyle = "plum";
         pctx.beginPath();
         pctx.arc(0, 0, 25, 0, .5*Math.PI);
         pctx.arc(0, 50, 25, 1.5*Math.PI, 0);
@@ -150,9 +167,9 @@ $(document).ready(function() {
         pctx.fill();
         break;
       case 6: 
-        pctx.fillStyle = 'whitesmoke'; 
+        pctx.fillStyle = "whitesmoke"; 
         pctx.fillRect(0, 0, 50, 50);
-        pctx.strokeStyle = 'seagreen';
+        pctx.strokeStyle = "seagreen";
         pctx.lineWidth = 6.25
         pctx.beginPath();
         pctx.arc(0, 0, 25, 0, 0.5*Math.PI);
@@ -180,9 +197,9 @@ $(document).ready(function() {
         pctx.stroke();
         break;
       case 7: 
-        pctx.fillStyle = 'powderblue'; 
+        pctx.fillStyle = "powderblue"; 
         pctx.fillRect(0, 0, 50, 50);
-        pctx.strokeStyle = 'crimson';
+        pctx.strokeStyle = "crimson";
         pctx.lineWidth = 4;
         pctx.beginPath();
         pctx.moveTo(-2, 0);
@@ -212,8 +229,60 @@ $(document).ready(function() {
   
   patternCt = (patternCt + 1) % 8;
   
-  let newPattern = ctx.createPattern(pattern, 'repeat');
+  let newPattern = ctx.createPattern(pattern, "repeat");
   ctx.fillStyle = newPattern;
   ctx.fillRect(0, 0, 500, 500);
   }
+  
+  $(".work-arrow").on("click", flipIfNeeded);
+  
+  function flipIfNeeded() {
+    let button = $(this);
+    if (rotationCt % 360 == 180) { 
+      flipit();
+      setTimeout(function() {
+        shiftWork(button);
+      }, 1000)
+    }
+    else shiftWork(button);
+  }
+  
+  function shiftWork(button) {
+    if (displayCt == 0) {
+      $backArrow.css("pointer-events", "auto");
+      $(".arrow-blocker").remove();
+    }
+    else if (displayCt == displayedWork.length - 1) {
+      $forwardArrow.css("pointer-events", "auto");
+      $(".arrow-blocker").remove();
+    }
+    let vw = window.innerWidth;
+    if (button.hasClass("forward-arrow")) {
+      $workBox.animate({ marginLeft: .095*vw }, 150, function() {
+        $(this).animate({ marginLeft: .1*vw }, 150);
+      });
+      displayCt += 1;
+    }
+    else {
+      $workBox.animate({ marginLeft: .105*vw }, 150, function() {
+        $(this).animate({ marginLeft: .1*vw }, 150);
+      });
+      displayCt -= 1;
+    }
+    
+    showWork();
+  }
+  
+  function showWork() {
+    $workItemFront.html(displayedWork[displayCt][0]);
+    $workItemBack.html(displayedWork[displayCt][1]);
+    if (displayCt == 0) {
+      $backArrow.css("pointer-events", "none");
+      $skewer.append("<div class='arrow-blocker' style='left:-24px'></div>");
+    }
+    else if (displayCt == displayedWork.length - 1) {
+      $forwardArrow.css("pointer-events", "none");
+      $skewer.append("<div class='arrow-blocker' style='right:-24px'></div>");
+    }
+  };
 });
